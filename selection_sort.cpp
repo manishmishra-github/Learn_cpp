d798ed7cf4e73aa8d7084b4e8fd70fe359472613
manishmishra-github <manishmishra.dev@gmail.com>
2025-07-07 01:27:11 +0100

    selection_sort.cpp

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -45,5 +45,78 @@ int main()
     {
         cout << "Element "<< i << " is " << arr[i] << endl;
     }
+
+
+cout << endl<<endl<<endl <<"Bubble sort"<<endl;
+
+    ///////////////Bubble sort
+
+    int barr[10] = {24,13,12,11,46,36,-1,90,100,0};
+    int bsize = sizeof(barr)/sizeof(barr[0]);
+    cout << "Size of array is " << bsize << endl;
+    for(int i = 0; i < bsize; i++)
+    {
+        cout << "Element "<< i << " is " << barr[i] << endl;
+    }
+
+    for(int i = size - 1; i >= 0; i--)
+    {
+        for(int j = 0; j < i; j++)
+        {
+            if(barr[j] > barr[j+1])
+            {
+                int temp = barr[j+1];
+                barr[j+1] = barr[j];
+                barr[j] = temp;
+            }
+        }
+    }
+
+    cout << endl << "After Sorting" << endl;
+
+    for(int i = 0; i < size; i++)
+    {
+        cout << "Element "<< i << " is " << barr[i] << endl;
+    }
+
+
+    cout << endl<<endl<<endl <<"Insertion sort"<<endl;
+
+    ///////////////Insertion sort
+
+    int iarr[6] = {1,2,3,4,5,6};
+    int isize = sizeof(iarr)/sizeof(iarr[0]);
+    cout << "Size of array is " << isize << endl;
+    for(int i = 0; i < isize; i++)
+    {
+        cout << "Element "<< i << " is " << iarr[i] << endl;
+    }
+
+    for(int i = 1; i < isize ; i++)
+    {
+        for(int j = i; j > 0; j --)
+        {
+            if(iarr[j] < iarr[j -1])
+            {
+                int temp = iarr[j -1];
+                iarr[j -1] = iarr[j];
+                iarr[j] = temp;
+            }
+            else
+            {
+                break;
+            }
+            cout << "runs"<< j<< endl;
+        }
+    }
+
+    cout << endl << "After Sorting" << endl;
+
+    for(int i = 0; i < isize; i++)
+    {
+        cout << "Element "<< i << " is " << iarr[i] << endl;
+    }
+
+
     return 0;
 }
